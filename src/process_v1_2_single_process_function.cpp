429f6a3bbc421dbef638435c621464b17ecbfffb
= <dario_dml@hotmail.com>
2015-03-05 11:50:04 +0000

    fixed v1_2

--- src/process_v1_2_single_process_function.cpp
+++ src/process_v1_2_single_process_function.cpp
@@ -182,23 +182,23 @@ void process(int levels, unsigned w, unsigned h, unsigned /*bits*/, std::vector<
 			
 			for(unsigned x=0;x<w;x++){
 				if(x==0){
-					out(0,0)=vmin(in(0,0), in(0,1), in(1,0));
+					out(0,0)=std::min(in(0,0), std::min(in(0,1), in(1,0)));
 					for(unsigned y=1;y<h-1;y++){
-						out(0,y)=vmin(in(0,y), in(0,y-1), in(1,y), in(0,y+1));
+						out(0,y)=std::min(in(0,y), std::min(in(0,y-1), std::min(in(1,y), in(0,y+1))));
 					}
-					out(0,h-1)=vmin(in(0,h-1), in(0,h-2), in(1,h-1));
+					out(0,h-1)=std::min(in(0,h-1), std::min(in(0,h-2), in(1,h-1)));
 				}else if(x<w-1){
-					out(x,0)=vmin(in(x,0), in(x-1,0), in(x,1), in(x+1,0));
+					out(x,0)=std::min(in(x,0), std::min(in(x-1,0), std::min(in(x,1), in(x+1,0))));
 					for(unsigned y=1;y<h-1;y++){
-						out(x,y)=vmin(in(x,y), in(x-1,y), in(x,y-1), in(x,y+1), in(x+1,y));
+						out(x,y)=std::min(in(x,y), std::min(in(x-1,y), std::min(in(x,y-1), in(x,y+1), in(x+1,y))));
 					}
-					out(x,h-1)=vmin(in(x,h-1), in(x-1,h-1), in(x,h-2), in(x+1,h-1));
+					out(x,h-1)=std::min(in(x,h-1), std::min(in(x-1,h-1), std::min(in(x,h-2), in(x+1,h-1))));
 				}else{
-					out(w-1,0)=vmin(in(w-1,0), in(w-1,1), in(w-2,0));
+					out(w-1,0)=std::min(in(w-1,0), std::min(in(w-1,1), in(w-2,0)));
 					for(unsigned y=1;y<h-1;y++){
-						out(w-1,y)=vmin(in(w-1,y), in(w-1,y-1), in(w-2,y), in(w-1,y+1));
+						out(w-1,y)=std::min(in(w-1,y), std::min(in(w-1,y-1), std::min(in(w-2,y), in(w-1,y+1))));
 					}
-					out(w-1,h-1)=vmin(in(w-1,h-1), in(w-1,h-2), in(w-2,h-1));
+					out(w-1,h-1)=std::min(in(w-1,h-1), std::min(in(w-1,h-2), in(w-2,h-1)));
 				}
 			}
 			std::swap(pixels, buffer);
@@ -209,26 +209,25 @@ void process(int levels, unsigned w, unsigned h, unsigned /*bits*/, std::vector<
 			
 			for(unsigned x=0;x<w;x++){
 				if(x==0){
-					out(0,0)=vmax(in(0,0), in(0,1), in(1,0));
+					out(0,0)=std::max(in(0,0), std::max(in(0,1), in(1,0)));
 					for(unsigned y=1;y<h-1;y++){
-						out(0,y)=vmax(in(0,y), in(0,y-1), in(1,y), in(0,y+1));
+						out(0,y)=std::max(in(0,y), std::max(in(0,y-1), std::max(in(1,y), in(0,y+1))));
 					}
-					out(0,h-1)=vmax(in(0,h-1), in(0,h-2), in(1,h-1));
+					out(0,h-1)=std::max(in(0,h-1), std::max(in(0,h-2), in(1,h-1)));
 				}else if(x<w-1){
-					out(x,0)=vmax(in(x,0), in(x-1,0), in(x,1), in(x+1,0));
+					out(x,0)=std::max(in(x,0), std::max(in(x-1,0), std::max(in(x,1), in(x+1,0))));
 					for(unsigned y=1;y<h-1;y++){
-						out(x,y)=vmax(in(x,y), in(x-1,y), in(x,y-1), in(x,y+1), in(x+1,y));
+						out(x,y)=std::max(in(x,y), std::max(in(x-1,y), std::max(in(x,y-1), in(x,y+1), in(x+1,y))));
 					}
-					out(x,h-1)=vmax(in(x,h-1), in(x-1,h-1), in(x,h-2), in(x+1,h-1));
+					out(x,h-1)=std::max(in(x,h-1), std::max(in(x-1,h-1), std::max(in(x,h-2), in(x+1,h-1))));
 				}else{
-					out(w-1,0)=vmax(in(w-1,0), in(w-1,1), in(w-2,0));
+					out(w-1,0)=std::max(in(w-1,0), std::max(in(w-1,1), in(w-2,0)));
 					for(unsigned y=1;y<h-1;y++){
-						out(w-1,y)=vmax(in(w-1,y), in(w-1,y-1), in(w-2,y), in(w-1,y+1));
+						out(w-1,y)=std::max(in(w-1,y), std::max(in(w-1,y-1), std::max(in(w-2,y), in(w-1,y+1))));
 					}
-					out(w-1,h-1)=vmax(in(w-1,h-1), in(w-1,h-2), in(w-2,h-1));
+					out(w-1,h-1)=std::max(in(w-1,h-1), std::max(in(w-1,h-2), in(w-2,h-1)));
 				}
 			}
-
 			std::swap(pixels, buffer);
 		}
 	} else {
@@ -238,26 +237,25 @@ void process(int levels, unsigned w, unsigned h, unsigned /*bits*/, std::vector<
 			
 			for(unsigned x=0;x<w;x++){
 				if(x==0){
-					out(0,0)=vmax(in(0,0), in(0,1), in(1,0));
+					out(0,0)=std::max(in(0,0), std::max(in(0,1), in(1,0)));
 					for(unsigned y=1;y<h-1;y++){
-						out(0,y)=vmax(in(0,y), in(0,y-1), in(1,y), in(0,y+1));
+						out(0,y)=std::max(in(0,y), std::max(in(0,y-1), std::max(in(1,y), in(0,y+1))));
 					}
-					out(0,h-1)=vmax(in(0,h-1), in(0,h-2), in(1,h-1));
+					out(0,h-1)=std::max(in(0,h-1), std::max(in(0,h-2), in(1,h-1)));
 				}else if(x<w-1){
-					out(x,0)=vmax(in(x,0), in(x-1,0), in(x,1), in(x+1,0));
+					out(x,0)=std::max(in(x,0), std::max(in(x-1,0), std::max(in(x,1), in(x+1,0))));
 					for(unsigned y=1;y<h-1;y++){
-						out(x,y)=vmax(in(x,y), in(x-1,y), in(x,y-1), in(x,y+1), in(x+1,y));
+						out(x,y)=std::max(in(x,y), std::max(in(x-1,y), std::max(in(x,y-1), in(x,y+1), in(x+1,y))));
 					}
-					out(x,h-1)=vmax(in(x,h-1), in(x-1,h-1), in(x,h-2), in(x+1,h-1));
+					out(x,h-1)=std::max(in(x,h-1), std::max(in(x-1,h-1), std::max(in(x,h-2), in(x+1,h-1))));
 				}else{
-					out(w-1,0)=vmax(in(w-1,0), in(w-1,1), in(w-2,0));
+					out(w-1,0)=std::max(in(w-1,0), std::max(in(w-1,1), in(w-2,0)));
 					for(unsigned y=1;y<h-1;y++){
-						out(w-1,y)=vmax(in(w-1,y), in(w-1,y-1), in(w-2,y), in(w-1,y+1));
+						out(w-1,y)=std::max(in(w-1,y), std::max(in(w-1,y-1), std::max(in(w-2,y), in(w-1,y+1))));
 					}
-					out(w-1,h-1)=vmax(in(w-1,h-1), in(w-1,h-2), in(w-2,h-1));
+					out(w-1,h-1)=std::max(in(w-1,h-1), std::max(in(w-1,h-2), in(w-2,h-1)));
 				}
 			}
-			
 			std::swap(pixels, buffer);
 		}
 		for(int i=0;i<std::abs(levels);i++){
@@ -266,23 +264,23 @@ void process(int levels, unsigned w, unsigned h, unsigned /*bits*/, std::vector<
 			
 			for(unsigned x=0;x<w;x++){
 				if(x==0){
-					out(0,0)=vmin(in(0,0), in(0,1), in(1,0));
+					out(0,0)=std::min(in(0,0), std::min(in(0,1), in(1,0)));
 					for(unsigned y=1;y<h-1;y++){
-						out(0,y)=vmin(in(0,y), in(0,y-1), in(1,y), in(0,y+1));
+						out(0,y)=std::min(in(0,y), std::min(in(0,y-1), std::min(in(1,y), in(0,y+1))));
 					}
-					out(0,h-1)=vmin(in(0,h-1), in(0,h-2), in(1,h-1));
+					out(0,h-1)=std::min(in(0,h-1), std::min(in(0,h-2), in(1,h-1)));
 				}else if(x<w-1){
-					out(x,0)=vmin(in(x,0), in(x-1,0), in(x,1), in(x+1,0));
+					out(x,0)=std::min(in(x,0), std::min(in(x-1,0), std::min(in(x,1), in(x+1,0))));
 					for(unsigned y=1;y<h-1;y++){
-						out(x,y)=vmin(in(x,y), in(x-1,y), in(x,y-1), in(x,y+1), in(x+1,y));
+						out(x,y)=std::min(in(x,y), std::min(in(x-1,y), std::min(in(x,y-1), in(x,y+1), in(x+1,y))));
 					}
-					out(x,h-1)=vmin(in(x,h-1), in(x-1,h-1), in(x,h-2), in(x+1,h-1));
+					out(x,h-1)=std::min(in(x,h-1), std::min(in(x-1,h-1), std::min(in(x,h-2), in(x+1,h-1))));
 				}else{
-					out(w-1,0)=vmin(in(w-1,0), in(w-1,1), in(w-2,0));
+					out(w-1,0)=std::min(in(w-1,0), std::min(in(w-1,1), in(w-2,0)));
 					for(unsigned y=1;y<h-1;y++){
-						out(w-1,y)=vmin(in(w-1,y), in(w-1,y-1), in(w-2,y), in(w-1,y+1));
+						out(w-1,y)=std::min(in(w-1,y), std::min(in(w-1,y-1), std::min(in(w-2,y), in(w-1,y+1))));
 					}
-					out(w-1,h-1)=vmin(in(w-1,h-1), in(w-1,h-2), in(w-2,h-1));
+					out(w-1,h-1)=std::min(in(w-1,h-1), std::min(in(w-1,h-2), in(w-2,h-1)));
 				}
 			}
 			std::swap(pixels, buffer);
