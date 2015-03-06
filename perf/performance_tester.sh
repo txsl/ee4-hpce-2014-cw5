0eee7d2030164bfa19914b17fbf05f958df7a3ad
Thomas Lim (AWS) <txl11@ic.ac.uk>
2015-03-06 22:39:28 +0000

    AWS data taken, and shell script modified slightly to play nicely

--- perf/performance_tester.sh
+++ perf/performance_tester.sh
@@ -2,19 +2,17 @@
 
 LENS="8 16 128 1024 8192 16384 32768 65536 131072 262144 524288 1048576 2097152 4194304 8388608";
 
-export PATH=`pwd`/../src:$PATH
-echo $PATH
+export HPCE_CL_SRC_DIR=`pwd`/../src
 
 echo "function_name,width,height,bits,levels,time" >> output.csv
 
 for w in $LENS; do
-    if [ `echo "$w*1 % 64" | bc` -eq 0 ]
-    then
+#    if [ `echo "${w}*1 % 64" | bc` -eq 0 ]
+#   then
         size=$(( w * h * 1 /8 ))
         echo $size $w $h;
         realtime=$(( time ( cat /dev/zero | head -c ${size} | ../bin/process_old ${w} ${w} 1 8 ) > /dev/null ) 2>&1 > /dev/null | grep real );
-        echo 'boo';
         echo $realtime;
-        echo "process_old,${w},${w},1,8,${realtime}"
-fi
+        echo "process_old,${w},${w},1,8,${realtime}" >> output.csv
+#fi
 done
