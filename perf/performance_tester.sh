6f529f4b407a8f8bef3d2957929a360cabccbe2f
Thomas Lim <txl11@ic.ac.uk>
2015-03-06 22:09:52 +0000

    Testing in squares for the time being

--- perf/performance_tester.sh
+++ perf/performance_tester.sh
@@ -8,15 +8,13 @@ echo $PATH
 echo "function_name,width,height,bits,levels,time" >> output.csv
 
 for w in $LENS; do
-    for h in $LENS; do
-        if [ `echo "$w*1 % 64" | bc` -eq 0 ]
-        then
-            size=$(( w * h * 1 /8 ))
-            echo $size $w $h;
-            realtime=$(( time ( cat /dev/zero | head -c ${size} | ../bin/process_old ${w} ${h} 1 8 ) > /dev/null ) 2>&1 > /dev/null | grep real );
-            echo 'boo';
-            echo $realtime;
-            echo "process_old,${w},${h},1,8,${realtime}"
-    fi
-    done
+    if [ `echo "$w*1 % 64" | bc` -eq 0 ]
+    then
+        size=$(( w * h * 1 /8 ))
+        echo $size $w $h;
+        realtime=$(( time ( cat /dev/zero | head -c ${size} | ../bin/process_old ${w} ${w} 1 8 ) > /dev/null ) 2>&1 > /dev/null | grep real );
+        echo 'boo';
+        echo $realtime;
+        echo "process_old,${w},${w},1,8,${realtime}"
+fi
 done
